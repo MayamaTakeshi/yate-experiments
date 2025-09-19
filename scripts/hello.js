22732a958d17cb50c3eb3b24a7317a091ffd8d0d
MayamaTakeshi <mayamatakeshi@gmail.com>
2025-09-19 21:16:44 +0900

    Solved recording issue

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,17 +1,3 @@
-
-// Strangely we get here only if we set
-// routing=hello.js
-// in conf.d/javascript.js
-
-// if we count on
-// ^1234$=javascript/hello.js
-// in the conf.d/regexroute.conf
-// we can see the route is resolved by the script is not executed.
-
-
-
-// the below doesn' work yet. We can hear the first and second prompts but recording ends after just one second.
 Channel.callTo("wave/play//usr/local/src/git/yate/audio/hello_good_morning.mulaw");
-Channel.callTo("wave/play//home/MayamaTakeshi/src/git/yate-experiments/hello_good_morning.mulaw");
-Channel.callTo("wave/record//root/tmp/echo.mulaw",{"maxlen": 10000, "blocking": 1, "nosilence": 1});
-Channel.callTo("wave/play//root/tmp/echo.mulaw");
+Channel.callTo("wave/record//tmp/recording.mulaw",{"maxlen": 80000, "blocking": 1, "nosilence": 1}); // attention maxlen is not duration! It is max number of bytes to be written to file.
+Channel.callTo("wave/play//tmp/recording.mulaw");
