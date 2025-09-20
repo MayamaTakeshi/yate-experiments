72ad1d7a6d086091663ddb5911740cdca680ce59
MayamaTakeshi <mayamatakeshi@gmail.com>
2025-09-21 06:33:33 +0900

    Added timeout for sipp in load tests

--- tests/load/start_sipp_uac.sh
+++ tests/load/start_sipp_uac.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-set -o errexit
+#set -o errexit
 set -o nounset
 set -o pipefail
 
-sudo sipp -i 127.0.0.1 -p 5050 -sf uac.xml -inf invite.txt 127.0.0.1:5060 -r 10
+while [[ 1 ]]
+do
+    sudo timeout --foreground -k 660 -s HUP 600 sipp -i 127.0.0.1 -p 5050 -sf uac.xml -inf invite.txt 127.0.0.1:5060 -r 10 -timeout 180s
+    reset
+    sleep 1
+done
 
--- tests/load/start_sipp_uas.sh
+++ tests/load/start_sipp_uas.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-set -o errexit
+#set -o errexit
 set -o nounset
 set -o pipefail
 
-sudo sipp -i 127.0.0.1 -p 5070 -sf uas.xml
+while [[ 1 ]]
+do
+    sudo timeout --foreground -k 660 -s HUP 600 sipp -i 127.0.0.1 -p 5070 -sf uas.xml -timeout 180s
+    reset
+    sleep 1
+done
+
